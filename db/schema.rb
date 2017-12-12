e90e07fc7ec19c8be21d04612ed3af283d86ca16
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-12 14:38:53 -0600

    Add validations for name and calories

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171212203354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "meals", force: :cascade do |t|
+    t.string "name"
+    t.integer "calories"
+    t.integer "protein"
+    t.integer "carbs"
+    t.integer "fat"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
