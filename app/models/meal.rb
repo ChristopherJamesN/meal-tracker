0e1d70b066f64283b8be8d98d823d8348ed6e8d4
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-12 19:18:50 -0600

    Add total count for all meals at bottom of meals table

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,36 @@
 class Meal < ApplicationRecord
-   validates :name, :calories,  :presence => true
+   validates :name, :calories, :protein, :carbs, :fat,  :presence => true
    belongs_to :user
+
+   def self.total_calories
+     calories = 0
+     Meal.all.each do |meal|
+       calories+= meal.calories
+     end
+     calories
+   end
+
+   def self.total_protein
+     protein = 0
+     Meal.all.each do |meal|
+       protein+= meal.protein
+     end
+     protein
+   end
+
+   def self.total_carbs
+     carbs = 0
+     Meal.all.each do |meal|
+       carbs+= meal.carbs
+     end
+     carbs
+   end
+
+   def self.total_fat
+     fat = 0
+     Meal.all.each do |meal|
+       fat+= meal.fat
+     end
+     fat
+   end
 end
